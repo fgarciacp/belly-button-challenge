8cf083cade70d68e932cf87d7ca89f975cef57de
fgarciacp <fgarciacp1@gmail.com>
2024-10-21 11:10:06 -0600

    app

---
+++ Starter_Code/static/js/app.js
@@ -0,0 +1,95 @@
+// Function to build both charts 
+function buildCharts(sample) {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+
+    // Get the samples field
+    const samples = data.samples;
+
+    // Filter the samples for the object with the desired sample number
+    const result = samples.filter(sampleObj => sampleObj.id == sample)[0];
+
+    // Get the otu_ids, otu_labels, and sample_values
+    const otu_ids = result.otu_ids;
+    const otu_labels = result.otu_labels;
+    const sample_values = result.sample_values;
+
+    // Build a Bubble Chart
+    const bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    }];
+
+    const bubbleLayout = {
+      title: "OTU IDs vs Sample Values",
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
+
+    // Render the Bubble Chart
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+    // For the Bar Chart, map the otu_ids to a list of strings for y-axis labels
+    const yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(); // <-- CORREGIDO
+    const barData = [{
+      x: sample_values.slice(0, 10).reverse(),
+      y: yticks,
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h"
+    }];
+
+    const barLayout = {
+      title: "Top 10 OTUs",
+      margin: { t: 30, l: 150 }
+    };
+
+    // Render the Bar Chart
+    Plotly.newPlot("bar", barData, barLayout);
+
+  });
+}
+
+// Function to run on page load
+function init() {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    // Get the names field
+    const sampleNames = data.names;
+
+    // Use d3 to select the dropdown with id of #selDataset
+    const selector = d3.select("#selDataset");
+
+    // Use the list of sample names to populate the select options
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Get the first sample from the list
+    const firstSample = sampleNames[0];
+
+    // Build charts and metadata panel with the first sample
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+// Function for event listener when a new sample is selected
+function optionChanged(newSample) {
+  // Build charts and metadata panel each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
